920c0cda827a65e515c23895d4eaf8cae20c3964
dikimiks <reb-polina@ya.ru>
2024-09-04 19:18:53 +0300

    задание 5

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let a = 10;
-let b = 2;
-alert(a + b);
-alert(a - b);
-alert(a * b);
-alert(a / b);
-let result = calculate(2, 5);
-alert(result);
-
+let a = 9; 
+let b = 2; 
+let remainder = a % b; 
+alert( + a  + b  + remainder);
 
