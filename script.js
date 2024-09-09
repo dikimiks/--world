0e4b410d6548d6a03e1798bf648c40733466ab6b
dikimiks <reb-polina@ya.ru>
2024-09-09 14:09:49 +0300

    Задание 1-6

--- script.js
+++ script.js
@@ -63,3 +63,41 @@ if (monthsNumber < 1 || monthsNumber > 12) {
     }
 }
 
+
+for (let i = 0; i < 2; i++) {
+    console.log("Привет");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 7; i <= 22; i++) {
+    console.log(i);
+}
+
+let obj = {
+    "Коля": '200',
+    "Вася": '300',
+    "Петя": '400'
+};
+
+for (let name in obj) {
+    console.log(`${name} — зарплата ${obj[name]} долларов`);
+}
+
+let n = 1000; 
+let num = 0;  
+while (n >= 50) {
+    n = n / 2; 
+    num++;      
+}
+console.log("Полученное число:", n);
+console.log("Количество итераций:", num);
+
+let firstFriday = 3; 
+const daysInMonth = 31;
+
+for (let day = firstFriday; day <= daysInMonth; day += 7) {
+    console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+}
