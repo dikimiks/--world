488ad5f18b25d9dc5fcb1c59a84f034e5af61e93
dikimiks <reb-polina@ya.ru>
2024-10-25 14:02:31 +0300

    доработки

---
+++ line.js
@@ -0,0 +1,8 @@
+@keyframes scroll-left {
+    from {
+        transform: translateX(100%); /* Начальная позиция за пределами справа */
+    }
+    to {
+        transform: translateX(-100%); /* Конечная позиция за пределами слева */
+    }
+}
